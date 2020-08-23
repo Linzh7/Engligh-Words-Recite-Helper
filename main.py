d4db91fa5bb5e3ed4f022448ec3b6ad723b45b02
Linzh <CatMo1999@gmail.com>
2020-08-23 11:57:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import csv
 import random
 import requests
+import os
 from playsound import playsound
 
-path = '.'
+# filepath
+path = os.getcwd()
 
 
+# get translated text from baidu
 def trans(word):
     url = 'https://fanyi.baidu.com/sug'
     headers = {
@@ -22,20 +25,24 @@ def trans(word):
     return res
 
 
+# print to screen
 def prints(element, degree):
     print('\033[1;32m  ', element[2], '\033[0m')
     print('\033[1;34m   熟练度:', element[3], '+', degree, '\033[0m')
     print()
 
 
+# read and test
 def do(ls):
     random.shuffle(ls)
     for element in ls:
+        # skip null element
         if element[1] == '':
             continue
         print('\033[1;33m', element[1], '\033[0m', end='           ')
         playsound(path + '/Audio/' + element[1] + '.mp3')
         know = input()
+        # get input
         if know in ['\'', '']:
             prints(element, 0)
         elif know in [';', '.']:
@@ -49,20 +56,27 @@ def do(ls):
             break
 
 
+# main
 ls = []
 donels = []
+# read from file
 with open(path + '/单词.csv', 'r') as f:
     lines = csv.reader(f)
     for i in lines:
         ls.append(i)
 
+# select mode
 mode = input("\033[1;35mMode: \033[0m")
+# main mode
 if mode in ['0', '']:
+    # print words amount
     print('\033[1;35msum:', len(ls), '\033[0m')
+    # exam
     try:
         do(ls)
     except:
         print("Error: 001")
+    # save unfinished words
     with open(path + '/单词.csv', 'w') as f:
         writer = csv.writer(f)
         for element in ls:
@@ -70,13 +84,16 @@ if mode in ['0', '']:
                 writer.writerow(element)
             else:
                 donels.append(element)
+    # save finished words
     with open(path + '/完成单词.csv', 'a+') as f:
         writer = csv.writer(f)
         for element in donels:
             writer.writerow(element)
     print("\033[1;33m[System]Saved.\033[0m")
 
+# modify mode
 elif mode == '9':
+    # download audio
     j = 1
     for i in ls:
         print("{}: {}".format(j, i[1]))
@@ -88,11 +105,15 @@ elif mode == '9':
             file.flush()
         j += 1
     print('\033[1;33m[System]MP3 Done.\033[0m')
+
+    # translate and format
     with open(path + '/单词.csv', 'w') as f:
         writer = csv.writer(f)
         for element in ls:
             if element[3] == '':
                 element[3] = '0'
+            if element[0] == '':
+                element[0] = '0'
             try:
                 element[2] = trans(element[1])
                 print(element[1], element[2])
